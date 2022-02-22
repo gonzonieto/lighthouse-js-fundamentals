8d03ced8252f62079ded17997fefa740b8d9c848
Gonzo <gonzonieto@gmail.com>
2022-02-22 06:16:17 +0000

    making odd checker

---
+++ odd.js
@@ -0,0 +1,12 @@
+function isOdd (num) {
+  if (num % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(isOdd(3));
+console.log(isOdd(4));
+console.log(isOdd(35));
+console.log(isOdd(36));
